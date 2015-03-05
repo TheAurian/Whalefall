8216a126698d9c8458b561e67414c66f94ddd502
TheAurian <hami@whalefallstudios.com>
2015-03-05 13:44:03 -0800

    completed rollaball minigame tut

---
+++ UnityTraining/Project1_Roll-a-ball/Assets/scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speedy;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+
+
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce(movement * speedy * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pickup")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString(); 
+		if (count >= 14){
+			winText.text = "You Win!";	
+		}
+	}
+}
---
+++ UnityTraining/Project1_Roll-a-ball/Assets/scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+    //not using forces, so can use update
+	void Update () 
+    {
+		transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+	}
+}
