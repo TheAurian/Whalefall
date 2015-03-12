ed588729e3e30b1ce30316aecb373b4417a6a47e
TheAurian <hami@whalefallstudios.com>
2015-03-11 20:25:06 -0700

    added asteroids and death anims asteroid destroy anims

---
+++ UnityTraining/SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject asteroidExplosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Boundary"))
+        {
+            return;
+        }
+     
+        Instantiate(asteroidExplosion, transform.position, transform.rotation);          
+        
+        if (other.tag.Equals("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
--- UnityTraining/SpaceShooter/Assets/Scripts/PlayerController.cs
+++ UnityTraining/SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -25,7 +25,6 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
 		}
 	}
 
