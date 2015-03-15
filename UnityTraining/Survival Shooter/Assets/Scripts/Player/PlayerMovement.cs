51072592748b9fa484b49c0075d383bc9d998d0c
TheAurian <hami@whalefallstudios.com>
2015-03-14 22:10:19 -0700

    added player movement and some audio clips + visual studio integration

--- UnityTraining/Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityTraining/Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,68 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("Floor");
+
+        // Set up references.
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    //runs every physics update (moved physics things here)
+    void FixedUpdate()
+    {
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        Move(h, v);
+
+        // Turn the player to face the mouse cursor.
+        Turning();
+
+        // Animate the player.
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h,0f,v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation (newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+
+        anim.SetBool("IsWalking", walking);
+    }
 }
